2480598dc8b44f33fc7cbd25c0c9f1eb2f6eb893
renanro919@gmail.com <renanro919@gmail.com>
2024-08-12 20:35:29 -0400

    fix: corrigindo bug de quebra de aplicação

--- src/layouts/_common/login-button.tsx
+++ src/layouts/_common/login-button.tsx
@@ -15,16 +15,25 @@ type Props = {
 };
 
 export default function LoginButton({ sx }: Props) {
-  const userAgent = typeof navigator === 'undefined' ? 'SSR' : navigator.userAgent;
+  const userAgent =
+    typeof navigator === 'undefined' ? 'SSR' : navigator.userAgent;
 
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      userAgent,
+    );
 
   return (
     <Button
       component={RouterLink}
-      href={`https://jiparana.lasercinemas.com.br/auth/login?mobile=${isMobile}`}
+      //href={`https://jiparana.lasercinemas.com.br/auth/login?mobile=${isMobile}`}
       variant="contained"
-      sx={{ mr: 1, ...sx, size: { xs: 'medium', md: 'small' }, height: { xs: 'auto', md: 35 } }}
+      sx={{
+        mr: 1,
+        ...sx,
+        size: { xs: 'medium', md: 'small' },
+        height: { xs: 'auto', md: 35 },
+      }}
     >
       Entrar
     </Button>
